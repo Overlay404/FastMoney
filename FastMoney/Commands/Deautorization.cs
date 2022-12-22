1302333acf0ae76a708acf5ab3806092755b2c37
Overlay404 <denis.alekseev.200411@gmail.com>
2022-12-22 09:48:25 +0300

    меленькие измнения

---
+++ FastMoney/Commands/Deautorization.cs
@@ -0,0 +1,21 @@
+﻿using FastMoney.Commands.Base;
+using FastMoney.View.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMoney.Commands
+{
+    internal class Deautorization : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            new AutorizationRegistration().Show();
+            MainWindow.Instanse.Close();
+        }
+    }
+}
---
+++ FastMoney/Commands/GoAutorizationPage.cs
@@ -0,0 +1,17 @@
+﻿using FastMoney.Commands.Base;
+using FastMoney.View.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMoney.Commands
+{
+    internal class GoAutorizationPage : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => AutorizationRegistration.Instance.frameAuthReg.Navigate(new View.Pages.Autorization());
+    }
+}
